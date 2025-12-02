3441d0b20715f6bb4e04a70385d79059acdc0d6c
malerinstitut <cvzg@malerinstitut.de>
2025-12-02 14:56:11 +0100

    V1

--- server.js
+++ server.js
@@ -11,8 +11,7 @@ app.use(cors());
 app.use(express.static(path.join(__dirname)));
 
 // Umgebungsvariablen für die API-URL (sollte in .env Datei gespeichert werden)
-// Fallback ist die aktuelle Web-App-URL des Google-Apps-Scripts
-const GOOGLE_SHEETS_API_URL = process.env.GOOGLE_SHEETS_API_URL || 'https://script.google.com/macros/s/AKfycbwgjlBIu5F8SrOkFSugbGE2DRq5jkezyZe7l8oApVdha5o0C8Y-kweN-V4-Lke5rED5/exec';
+const GOOGLE_SHEETS_API_URL = process.env.GOOGLE_SHEETS_API_URL || 'https://script.google.com/a/macros/moseler.de/s/AKfycbwgjlBIu5F8SrOkFSugbGE2DRq5jkezyZe7l8oApVdha5o0C8Y-kweN-V4-Lke5rED5/exec';
 
 // Proxy-Endpoint für Projekte
 app.get('/api/projects', async (req, res) => {
